e8c0d8202b6c4e709be1182e56d8cab9ffc00a4e
Maruf Khan <whoismaruf@outlook.com>
2020-03-30 23:34:23 +0600

    added food app

---
+++ food/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Item(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+
+
+class Package(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    item_list = models.TextField(serialize=True)
---
+++ food/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Item, Package
+# Create your views here.
+
+def food(request):
+    items = [item for item in Item.objects.all()]
+    data = {
+        'title': 'Foods',
+        'items': items[:3],
+    }
+    return render(request, 'food/index.html', context=data)
+
+
+def package(request):
+    packages = [pack for pack in Package.objects.all()]
+
+    data = {
+        'title': 'Packages',
+        'packages1': {
+            'name': packages[0].name,
+            'price': packages[0].price,
+            'item_list': packages[0].item_list
+        },
+        'packages2': {
+            'name': packages[1].name,
+            'price': packages[1].price,
+            'item_list': packages[1].item_list
+        },
+        'packages3': {
+            'name': packages[2].name,
+            'price': packages[2].price,
+            'item_list': packages[2].item_list
+        }
+    }
+    return render(request, 'package/index.html', context=data)
+
+
+def contact(request):
+    return render(request, 'contact/index.html')
